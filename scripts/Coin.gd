8ed8438869d46ffbe38f4707cc1f4e4ceab14da6
JwalaJ <jwalarose06@gmail.com>
2026-02-28 21:15:18 +0000

    Spinning coin

---
+++ scripts/Coin.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+#size of the spawn area
+@export var area_width: float = 20.0
+@export var area_depth: float = 20.0
+@export var min_height: float = 0.5
+@export var max_height: float = 0.5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize() #ensures random positions 
+	position = Vector3(
+		randf_range(-area_width/2, area_width/2),
+		randf_range(min_height, max_height),
+		randf_range(area_depth/2, area_depth/2)		
+	)
+	
+
+
+func _on_body_entered(body):
+	queue_free() 
