04a55282b7b8be45db8f22e56e3a5a7564bf9ef2
Layton <laytonb@hotmail.co.uk>
2026-02-28 22:58:48 +0000

    CharacterContainer: trap mouse until Esc is pressed

--- Scenes/Character/character_container.gd
+++ Scenes/Character/character_container.gd
@@ -2,5 +2,17 @@ extends SubViewportContainer
 
 @onready var test_world: TestWorld = %test_world
 
+var keep_pointer_centered := true :
+	set(value):
+		keep_pointer_centered = value
+		if value:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_menu"):
+		keep_pointer_centered = !keep_pointer_centered
+		return
+	
 	test_world.character._unhandled_input(event)
