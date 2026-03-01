563eeb1571655f0cb4e1973e0cdbd2b83af8eb55
Layton <laytonb@hotmail.co.uk>
2026-03-01 07:55:48 +0000

    VideoManager: change hand video to match state

--- ui_left_hand_video_manager.gd
+++ ui_left_hand_video_manager.gd
@@ -13,5 +13,21 @@ func _ready() -> void:
 	CharacterState.state_change.connect(_on_character_state_state_change)
 
 
-func _on_character_state_state_change(new_state: CharacterState.State, prev_state: CharacterState.State) -> void:
-	pass
+func _on_character_state_state_change(new_state: CharacterState.State, _prev_state: CharacterState.State) -> void:
+	match new_state:
+		CharacterState.State.STATIONARY:
+			self.stream = _vid_stationary
+		CharacterState.State.WALKING:
+			self._play(_vid_walk)
+		CharacterState.State.CROUCHING:
+			self._play(_vid_crouch)
+		CharacterState.State.SPRINTING:
+			self._play(_vid_sprint)
+		CharacterState.State.JUMPING:
+			self._play(_vid_jump, false)
+
+
+func _play(new_stream: VideoStreamTheora, do_loop: bool = true) -> void:
+	self.stream = new_stream
+	self.loop = do_loop
+	self.play()
